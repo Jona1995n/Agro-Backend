31f4402ca1264737814acf1cbabea5fa7334bac8
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-21 00:12:05 -0400

    setting up

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+reviews = [
+	{
+  		"body": "Ty yuuuu teeth kitty Uighur ewryub Kiki dusty jujus huiii ffghhh ccddjk kkkkjbbdd Ruth just gddgii",
+  		"email": "j@n-com",
+  		"name": "Jona Nunez",
+  		"stars": 3,
+  		"time": 1686539559,
+  		"title": "Ok!!!!!"
+	},
+	{
+  		"body": "yerrrrrrrrrrrr",
+  		"email": "j@n-com",
+  		"name": "Jona Nunez",
+  		"stars": 5,
+  		"time": 1686539400,
+  		"title": "Ok!!!!!"
+	}
+]
+
 def home(request):
-	return HttpResponse('<h1>Reviews Home</h1>')
+	context = {
+		'title': 'Reviews',
+		'reviews': reviews
+	}
+
+	return render(request, 'Agro/reviews.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Reviews About</h1>')
+	return render(request, 'Agro/about.html')
+
