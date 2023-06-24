7c6ab425619301e3595a2ae3384a7e59c3b89f05
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-24 00:23:57 -0400

    heroku updates

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ class Facility(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    lat = models.DecimalField(max_digits=5, decimal_places=5, default=0)
-    lon = models.DecimalField(max_digits=5, decimal_places=5, default=0)
+    lat = models.DecimalField(max_digits=10, decimal_places=5, default=0)
+    lon = models.DecimalField(max_digits=10, decimal_places=5, default=0)
 
     def __str__(self):
         return self.name
