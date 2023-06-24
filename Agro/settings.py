0aed89f458396e28ad02ad2a35df3501b621d886
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-24 01:55:24 -0400

    heroku updates

--- Agro/settings.py
+++ Agro/settings.py
@@ -93,7 +93,7 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
