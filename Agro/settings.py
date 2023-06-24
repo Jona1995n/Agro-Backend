9864abd34ce2926ad69d48353b1a9b19521f46d6
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-24 02:15:09 -0400

    heroku updates

--- Agro/settings.py
+++ Agro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'rest_framework'
 ]
 
@@ -91,10 +92,10 @@ DATABASES['default'] = db_env
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework.authentication.TokenAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
     #     'rest_framework.authentication.SessionAuthentication',
-    # ),
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
