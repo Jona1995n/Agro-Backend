c98d667235e1d1c640606b0d2dc9731a917e9d47
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-25 14:16:05 -0400

    adding review model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Facility(models.Model):
     name = models.CharField(max_length=100)
