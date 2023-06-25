8c4771532708f5004f6b255942f736357e6ee83d
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-25 14:21:21 -0400

    adding review model

--- reviews/models.py
+++ reviews/models.py
@@ -3,10 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Review(models.Model):
-	title = models.CharField(max_length=30)
-	body = models.TextField()
-	time = models.DateTimeField(default=timezone.now)
-	name = models.ForeignKey(User, on_delete=models.CASCADE)
+	reviewBody = models.CharField(max_length=30)
 
 	def __str__(self):
 		return self.title
