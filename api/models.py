36b134dd1c18d9d76c7b97e82e75c7394f308707
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-26 23:14:55 -0400

    django editing Review model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 class Review(models.Model):
     title = models.CharField(max_length=30)
@@ -13,13 +14,16 @@ class Review(models.Model):
     def __str__(self):
 	    return self.title
 
+class ReviewInlines(admin.TabularInline):
+    model = Review
+
 class Facility(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     lat = models.DecimalField(max_digits=10, decimal_places=5, default=0)
     lon = models.DecimalField(max_digits=10, decimal_places=5, default=0)
-    reviews = Review
+    reviews = [ReviewInlines]
 
     def __str__(self):
         return self.name
