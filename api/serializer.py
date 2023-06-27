bdeb9f9bb85d086a66a308e8465cbd63fd5cc290
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-27 14:59:26 -0400

    django editing Review model

--- api/serializer.py
+++ api/serializer.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from .models import Facility, Review
 
-class FacilitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Facility
-        fields = ['name', 'category', 'address', 'lat', 'lon']
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['title', 'body', 'time', 'stars', 'email', 'user']
+        fields = ['title', 
+                  'body', 
+                  'time', 
+                  'stars', 
+                  'email', 
+                  'user'
+                  ]
+
+class FacilitySerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True) 
+
+    class Meta:
+        model = Facility
+        fields = ['name', 
+                  'category', 
+                  'address', 
+                  'lat', 
+                  'lon',
+                  'reviews'
+                  ]
