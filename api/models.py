34a63ee49609f19a372d2757f5ffc68724c7ae48
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-27 00:06:36 -0400

    django editing Review model

--- api/models.py
+++ api/models.py
@@ -19,7 +19,7 @@ class Review(models.Model):
     stars = models.IntegerField()
     email = models.CharField(max_length=50)
     user = models.CharField(max_length=50)
-    # facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE, default='')
 
     def __str__(self):
 	    return self.title   
