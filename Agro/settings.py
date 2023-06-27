e834076bba7b676956c43ec7d049e28366271a42
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-26 20:47:14 -0400

    django editing Review model

--- Agro/settings.py
+++ Agro/settings.py
@@ -99,7 +99,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DATETIME_FORMAT': '%s000'
+    'DATETIME_FORMAT': '%s'
 }
 
 # Password validation
