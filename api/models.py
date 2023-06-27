5648c397b71084db9aafdc575c8d21e96a9686c6
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-27 16:02:55 -0400

    django editing Review model

--- api/models.py
+++ api/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Facility(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    lat = models.DecimalField(max_digits=10, decimal_places=5, default=0)
-    lon = models.DecimalField(max_digits=10, decimal_places=5, default=0)
-
-    def __str__(self):
-        return self.name
-
 class Review(models.Model):
     title = models.CharField(max_length=30)
     body = models.TextField()
@@ -21,4 +11,15 @@ class Review(models.Model):
     user = models.CharField(max_length=50)
 
     def __str__(self):
-	    return self.title   
+	    return self.title  
+
+class Facility(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    lat = models.DecimalField(max_digits=10, decimal_places=5, default=0)
+    lon = models.DecimalField(max_digits=10, decimal_places=5, default=0)
+    reviews = models.ManyToManyField(Review)
+
+    def __str__(self):
+        return self.name 
