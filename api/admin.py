f56ce2980617181911847fee098e0e7711a009a5
Jonathan Nunez <Jonathannunez16@gmail.com>
2023-06-26 23:22:43 -0400

    django editing Review model

--- api/admin.py
+++ api/admin.py
@@ -3,3 +3,9 @@ from .models import Facility, Review
 
 admin.site.register(Facility)
 admin.site.register(Review)
+
+class EntryInlines(admin.TabularInline):
+    model = Review
+
+class FacilityAdmin(admin.ModelAdmin):
+    inlines = [EntryInlines]
--- api/models.py
+++ api/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.contrib import admin
+
+class Facility(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    lat = models.DecimalField(max_digits=10, decimal_places=5, default=0)
+    lon = models.DecimalField(max_digits=10, decimal_places=5, default=0)
+
+    def __str__(self):
+        return self.name
 
 class Review(models.Model):
     title = models.CharField(max_length=30)
@@ -13,17 +22,3 @@ class Review(models.Model):
 
     def __str__(self):
 	    return self.title
-
-class ReviewInlines(admin.TabularInline):
-    model = Review
-
-class Facility(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    lat = models.DecimalField(max_digits=10, decimal_places=5, default=0)
-    lon = models.DecimalField(max_digits=10, decimal_places=5, default=0)
-    reviews = [ReviewInlines]
-
-    def __str__(self):
-        return self.name
